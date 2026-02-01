960de24f21f053d9145add6353164981cf5835b3
shanikumar001 <shanikumar00231@gmail.com>
2026-02-01 18:04:09 +0530

    adding backend url

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://project-gallery-backend-yra4.onrender.com',
         changeOrigin: true,
       },
     },
